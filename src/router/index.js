3f607fa3c733543dc1314b394e2b7e5694214f8d
YtMour <ResidualStar61@gmail.com>
2024-04-08 18:08:01 +0800

    大事件-目录调整

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// createRouter 创建路由实例，===> new VueRouter()
+// 1. history模式: createWebHistory()   http://xxx/user
+// 2. hash模式: createWebHashHistory()  http://xxx/#/user
+
+// vite 的配置 import.meta.env.BASE_URL 是路由的基准地址，默认是 ’/‘
+// https://vitejs.dev/guide/build.html#public-base-path
+
+// 如果将来你部署的域名路径是：http://xxx/my-path/user
+// vite.config.ts  添加配置  base: my-path，路由这就会加上 my-path 前缀了
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: []
+})
+
+export default router
